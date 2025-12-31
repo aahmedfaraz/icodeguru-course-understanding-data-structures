b2b8591d7cadca1731700925667b00c937020aae
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2025-12-31 23:44:13 +0500

    Add Code Week 1 Day 1

---
+++ week1-day1/script.py
@@ -0,0 +1,27 @@
+# Index    0   1   2   3   4   5
+numbers = [10, 20, 30, 40, 30, 50]
+
+# Access
+print(numbers[2]) # O(1)
+
+# Insert
+numbers.append(60) # O(1) amortized
+numbers.insert(0, 5) # O(n)
+numbers.insert(2, 15) # O(n)
+print(numbers)
+
+# Remove
+numbers.pop() # O(1)
+print(numbers)
+numbers.pop(0) # O(n)
+print(numbers)
+numbers.remove(30) # O(n)
+print(numbers)
+
+# Update
+numbers[4] = 35 # O(1)
+print(numbers)
+
+# Traverse
+for num in numbers: # O(n)
+  print(num, '->', num * num)
